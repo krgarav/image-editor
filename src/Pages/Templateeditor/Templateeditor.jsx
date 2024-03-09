d769d97bb57a4b580c2ebb1c4db76e0e28167611
krgarav <krgarav@gmail.com>
2024-03-09 21:18:36 +0530

    merge with shivam

--- src/Pages/Templateeditor/Templateeditor.jsx
+++ src/Pages/Templateeditor/Templateeditor.jsx
@@ -21,26 +21,40 @@ function Templateeditor(props) {
   let newArray = [];
   let index = 0;
   const getCroppImageHandler = (index) => {
+    console.log(index);
     navigate("/imgeditor");
   };
-
+const border="1px solid grey"
   for (let index = 0; index < totalColumns; index++) {
     newArray.push(
       <div
         key={index}
-        className="cols border d-flex justify-content-center align-items-center fw-bolder"
+        className={`cols  d-flex justify-content-center align-items-center fw-bolder p-5`}
         style={{
-          height: "250px",
+          
+          border: !urlOfArray[index] ? border : "none",
           backgroundImage: `url(${urlOfArray[index]})`,
+          backgroundSize:
+            "100% 100%" /* Cover will ensure the image covers the entire div */,
+          backgroundPosition: "center" /* Center the background image */,
+          backgroundRepeat: "no-repeat" /* Prevent the image from repeating */,
         }}
         onClick={() => getCroppImageHandler(index)}
       >
-        +
+       {!urlOfArray[index] && "+"} 
       </div>
     );
   }
 
   const handleDownload = () => {
+    // if (mergedImage) {
+    //   // Create an anchor element
+    //   const link = document.createElement("a");
+    //   link.href = mergedImage;
+    //   link.download = "merged_image.png";
+    //   // Simulate click on the anchor element to trigger download
+    //   link.click();
+    // }
     const collageElement = document.querySelector("#collage");
     html2canvas(collageElement).then((canvas) => {
       const link = document.createElement("a");
@@ -56,15 +70,14 @@ function Templateeditor(props) {
       style={{ width: "100vw", height: "100vh" }}
     >
       <div
+        className={`row row-cols-${perLineCols} m-3 border`}
         id="collage"
-        className={`row row-cols-${perLineCols}`}
-        style={{ width: "280px" }}
+        style={{ Width: "600px", height: "400px" }}
       >
         {newArray}
       </div>
       <div className="row text-center">
         <div className="btn btn-warning my-5" onClick={handleDownload}>
-          {" "}
           download merge image
         </div>
       </div>
