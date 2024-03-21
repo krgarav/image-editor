29e5ab5f318bad615693b0d048c4a3030a31d831
krgarav <krgarav@gmail.com>
2024-03-21 15:45:00 +0530

    completed image editor functionality

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Imageprovider from "./store/Image-provider.jsx";
-import { BrowserRouter } from "react-router-dom";
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from "react-toastify";
 ReactDOM.createRoot(document.getElementById("root")).render(
